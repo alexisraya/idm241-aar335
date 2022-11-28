a69ac02fc2c9194c2f9a0d98c6a8101b785035c8
alexisraya <aar335@drexel.edu>
2022-11-27 22:46:52 -0500

    :construction: click outside button function implemented

--- final/final build/main.js
+++ final/final build/main.js
@@ -1,6 +1,6 @@
 const addButton = document.querySelector(".js-btn");
 const buttonText = document.querySelector(".js-text");
-const finalItems = document.querySelector(".js-items")
+const finalItems = document.querySelector(".js-items");
 
 const minusSign = document.querySelector(".js-minus");
 const plusSign = document.querySelector(".js-plus");
@@ -8,10 +8,10 @@ const itemAmmount = document.querySelector(".js-value");
 
 const clickField = document.querySelector(".js-box");
 
+
 let ammount = 1;
 let hasChanged = false;
-let clickedField = false;
-let clickedButton = false;
+let isDefault = true;
 
 function grow(){
    if (hasChanged == false){
@@ -30,6 +30,19 @@ function reset(){
   finalItems.style.display = "none";
   buttonText.style.display = "block";
   addButton.classList.remove("add-btn-grow");
+}
+
+let handleClickOutsideElement = function  (elm, callback) {
+  
+  var fnOutsideElement = function (event) {
+    if (event.target !== elm && !elm.contains(event.target)) {
+      callback();
+      // detaches automatically after usage
+      document.removeEventListener('click', fnOutsideElement, true);
+    }
+  }
+
+  document.addEventListener('click', fnOutsideElement, true);
 }
 
 // clickField.addEventListener('click', () => {
@@ -51,7 +64,6 @@ function reset(){
 
 addButton.addEventListener('click', () => {
     clickedButton = true;
-    clickedField = false;
     grow();
 });
 
@@ -70,4 +82,26 @@ minusSign.addEventListener('click', () => {
     itemAmmount.innerHTML = ammount.toString();
   }
   
+});
+
+document.addEventListener('click', function(event) {
+  let isClickInside = clickField.contains(event.target);
+  if (isClickInside) {
+    if(isDefault == false){
+      addButton.classList.remove("outside");
+      grow();
+      minusSign.style.display = "flex";
+      plusSign.style.display = "flex";
+      itemAmmount.classList.add("value-label");
+    }
+  }
+  else{
+    if(buttonText.style.display == "none"){
+      isDefault = false;
+      minusSign.style.display = "none";
+      plusSign.style.display = "none";
+      addButton.classList.add("outside");
+      itemAmmount.classList.remove("value-label");
+    }
+  }
 });
